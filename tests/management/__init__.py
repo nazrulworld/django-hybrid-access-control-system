f6d4e0f0ea6f1bee4e4b6e27364874e7c4c62828
Md Nazrul Islam <email2nazrul@gmail.com>
2016-06-26 13:51:48 +0200

    individual managemnet commad testcase are moved to management package. fix: fixture path after moving

---
+++ tests/management/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# ++ This file `__init__.py.py` is generated at 6/24/16 7:00 PM ++
+
+__author__ = "Md Nazrul Islam<connect2nazrul@gmail.com>"
---
+++ tests/management/test_init_hacs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# ++ This file `test_management_init_hacs.py` is generated at 5/17/16 4:48 PM ++
+import os
+from django.test import TestCase
+from django.contrib.sites.models import Site
+from django.core.management import call_command
+
+from hacs.models import RoutingTable
+from hacs.models import SiteRoutingRules
+
+__author__ = "Md Nazrul Islam<connect2nazrul@gmail.com>"
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestInitHACS(TestCase):
+    """ """
+
+    def setUp(self):
+
+        super(TestInitHACS, self).setUp()
+
+    def clean(self):
+        """ Cleaning all existing Routing records those are comes with fixture """
+        SiteRoutingRules.objects.all().delete()
+        RoutingTable.objects.all().delete()
+
+    def test_handle(self):
+        """ """
+
+        call_command('init_hacs', domain_name='localhost', urlconf_module='hacs.urls', route_name='default-route')
+
+        # we make sure HACS is initialized
+        try:
+            RoutingTable.objects.get_by_natural_key('default-route')
+        except RoutingTable.DoesNotExist:
+            raise AssertionError("Code should come here!, cause route should be created")
+
+        try:
+            site_route = SiteRoutingRules.objects.get(site=Site.objects.get(domain='localhost'))
+            self.assertEqual(site_route.route.route_name, 'default-route')
+
+        except SiteRoutingRules.DoesNotExist:
+            raise AssertionError("Code should come here!, cause site route should be created")
+
+    def tearDown(self):
+
+        super(TestInitHACS, self).tearDown()
